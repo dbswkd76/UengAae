4fd40e2453eaab6afa3649d2889e16d637e09cea
meoheee <dany1124@hanmail.net>
2021-03-27 21:48:31 +0900

    머히태그

---
+++ game/Assets/04.Script/meoheeeTag.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class meoheeeTag : MonoBehaviour
+{
+    public SpriteRenderer pimmbung;
+    public SpriteRenderer bungpimm;
+    private bool istag = true;
+
+    // Update is called once per frame
+    void Update()
+    {
+        Player_Tag();
+    }
+
+    public void Player_Tag()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            if (istag == true)
+            {
+                pimmbung.enabled = false;
+                bungpimm.enabled = true;
+                istag = false;
+                Debug.Log("붕핌이 달려!!");
+            }
+            else
+            {
+                pimmbung.enabled = true;
+                bungpimm.enabled = false;
+                istag = true;
+                Debug.Log("핌붕이 달려!!");
+            }
+
+        }
+    }
+
+}
