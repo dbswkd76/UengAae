f8c62df1d71f64674b591aa67843e256cb40b6d9
meoheee <dany1124@hanmail.net>
2021-03-31 00:30:21 +0900

    21.03.31.01

--- game/Assets/04.Script/meoheeeTag.cs
+++ game/Assets/04.Script/meoheeeTag.cs
@@ -6,6 +6,8 @@ public class meoheeeTag : MonoBehaviour
 {
     public SpriteRenderer pimmbung;
     public SpriteRenderer bungpimm;
+    public AudioSource tag_sound;
+    public bool tag_able = true;
     private bool istag = true;
 
     // Update is called once per frame
@@ -16,7 +18,7 @@ public class meoheeeTag : MonoBehaviour
 
     public void Player_Tag()
     {
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetKeyDown(KeyCode.LeftAlt) && tag_able == true)
         {
             if (istag == true)
             {
@@ -32,8 +34,23 @@ public class meoheeeTag : MonoBehaviour
                 istag = true;
                 Debug.Log("핌붕이 달려!!");
             }
-
+            if (tag_sound != null)
+            {
+                tag_sound.Play();
+            }
         }
+    }
+
+    public void OnCollisionEnter2D()
+    {
+        tag_able = true;
+        Debug.Log("점프 안하는 중");
+    }
+
+    public void OnCollisionExit2D()
+    {
+        tag_able = false;
+        Debug.Log("점프 하는 중");
     }
 
 }
