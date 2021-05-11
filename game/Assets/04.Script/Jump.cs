f9df488b79b2ca9c9b14456d1b5136d6e7de246a
dbswkd76 <77269556+dbswkd76@users.noreply.github.com>
2021-05-11 20:33:33 +0900

    jump
    점프입니다

---
+++ game/Assets/04.Script/Jump.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+public class Jump : MonoBehaviour
+{
+    Rigidbody2D myrigidbody;
+
+    [SerializeField] float power;
+    [SerializeField] Transform pos;
+    [SerializeField] float checkRadius;
+    [SerializeField] LayerMask islayer;
+
+    public int jumpCount;
+    int jumpCnt;
+
+    bool isGround;
+
+
+    private void Start()
+    {
+        myrigidbody = GetComponent<Rigidbody2D>();
+        jumpCnt = jumpCount;
+        
+    }
+
+     void Update()
+    {
+        isGround = Physics2D.OverlapCircle(pos.position, checkRadius, islayer);
+
+        if (isGround == true && Input.GetKeyDown(KeyCode.Space) && jumpCnt > 0)
+        {
+            
+            myrigidbody.velocity = Vector2.up * power;
+        }
+        if (isGround == false && Input.GetKeyDown(KeyCode.Space) && jumpCnt > 0)
+        {
+
+            myrigidbody.velocity = Vector2.up * power;
+        }
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            jumpCnt--;
+        }
+        if (isGround)
+        {
+            jumpCnt = jumpCount;
+        }
+    }
+
+}
+
