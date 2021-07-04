09718eaa0a26661a133185d28b739b7ac52cd061
meoheee <182330@jnu.ac.kr>
2021-07-04 18:10:42 +0900

    21.07.04.01

--- game/Assets/04.Script/Timer.cs
+++ game/Assets/04.Script/Timer.cs
@@ -4,13 +4,27 @@ using UnityEngine;
 
 public class Timer : MonoBehaviour
 {
+    public GameObject[] timers;
     void Start()
     {
-        
+        StartCoroutine(StartTimer());
     }
 
-    void Update()
+    IEnumerator StartTimer()
     {
-        
+        Time.timeScale = 0f;
+        yield return StartCoroutine(ChangeUi());
+        Time.timeScale = 1f;
+    }
+
+    IEnumerator ChangeUi()
+    {
+        for (int i = 0; i < 3; i++)
+        {
+            timers[i].SetActive(true);
+            yield return new WaitForSecondsRealtime(0.5f);
+            timers[i].SetActive(false);
+        }
+
     }
 }
