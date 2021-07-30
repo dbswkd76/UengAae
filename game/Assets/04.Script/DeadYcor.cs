17c68cb21c4162719e8ed76e7c4d1ebd484f47d2
meoheee <dany1124@hanmail.net>
2021-07-31 00:44:32 +0900

    21.07.31.01

--- game/Assets/04.Script/DeadYcor.cs
+++ game/Assets/04.Script/DeadYcor.cs
@@ -1,16 +1,27 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class DeadYcor : MonoBehaviour {
+    public AudioSource diesound;
+    public GameObject music;
+    Animator anim;
 
+    void Awake() {
+        anim = GetComponent<Animator>();
+    }
 
-
-
-    // Update is called once per frame
     void Update()
     {
-        
+        if (!GameManager.playerDie){
+            if (transform.position.y>10){
+                GameManager.playerDie = true;
+                music.SetActive(false);
+                Debug.Log("Die");
+                anim.SetBool("isDie", true);
+                diesound.Play();
+            }
+        }
     }
 }
